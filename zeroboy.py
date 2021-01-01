adac9a0a4889584d75db54fdf5be4f3dd13ae182
NawabDaDx <76843819+NawabDaDx@users.noreply.github.com>
2021-01-01 20:53:17 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+|__(~)    •||•THE - NAWAB -OFFICAL------•◈•
+|__\~~) •||•NAWAB - AYASH---------------•◈•
+|__(-----\  •◈•------FCBOK-STARx--------•◈•
+|__~~~\ •◈•-----█-------✪-------█------•◈•
+|__~~~\ •◈•-----█-------✪-------█------•◈•
+|__~~~\ •◈•-----█-------✪-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  NAWAB ZADA 
+\033[
+\033[1;96mFacebook  \https://www.facebook.com/NAwAbDadX
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Nawabo/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor NAWAB ZADA
+\033[1;96mInstagram \033[1;93m: \033[1;92mFolloNawab
+\033[1;96mFacebook  https://www.facebook.com/NAwAbDadX
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/nawabo/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "NAWABO"
+CorrectPassword = "AYASHO"
 
 loop = 'true'
 while (loop == 'true'):
